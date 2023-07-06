a115d11e64fd7d3415e424d9a92689c88b1b95d3
Simon Hagelin <hagelin05@gmail.com>
2023-07-06 13:45:46 +0200

    v2

--- mainN.py
+++ mainN.py
@@ -1,17 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-n_array = np.arange(1, 2, 0.001) #Creates list of refractive indices
+N_array = np.arange(1, 1001, 1) #Creates list of fringe shifts
 
-def michelson_calc (T, theta_i, theta_f, wavelength, N):
+def michelson_calc (T, theta_i, theta_f, wavelength, n):
     wavelength *= 10**(-6) #Nanometers to millimeters
     theta_i = np.deg2rad(theta_i)
     theta_f = np.deg2rad(theta_f) #Degrees to radians
 
     value_array = []
 
-    # Calculates both functions for all values of n
-    for n in n_array:
+    # Calculates both functions for all values of N
+    for N in N_array:
         sqrt_i = np.sqrt(1-((np.sin(theta_i)**2)/n**2))
         left_i =  -(2 * T / sqrt_i) * np.cos(theta_i - np.arccos(sqrt_i))
         right_i = (2 * n * T) / sqrt_i
@@ -28,12 +28,13 @@ def michelson_calc (T, theta_i, theta_f, wavelength, N):
 
     # Finds the refractive index used in the previous equation where the answer
     # was closest to zero.
-    index = np.interp(0, value_array, n_array)
-    print('Index: ' + str(index))
+    fringeCount = np.interp(0, value_array, N_array)
+    print(len(value_array) + ' ' + len(N_array_)
+    print('Number of fringe counts: ' + str(fringeCount))
 
     # Plots a graph that visualise the calculations
     fig, ax = plt.subplots()
-    ax.plot(n_array,value_array)
+    ax.plot(N_array,value_array)
     ax.axis
     ax.axhline(c='grey', lw=1)
 
@@ -41,7 +42,7 @@ def michelson_calc (T, theta_i, theta_f, wavelength, N):
     plt.rc('text.latex', preamble=r'\usepackage{textgreek}')
 
     plt.ylabel(r'$\dfrac{f (θ_f) - f (θ_i) }{λ}$')
-    plt.xlabel('Refractive index')
+    plt.xlabel('Refractive fringeCount')
     plt.subplots_adjust(left=0.15, bottom=0.1)
     plt.savefig('michelsonGraph.png', )
     plt.show()
